5ca73de4a1af693b77b47f4b1f7f87cd2d4c93f3
native-iron-man <862046184@qq.com>
2025-03-07 13:10:21 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("参数错误：需要三个文件路径参数");
+            return;
+        }
+
+    }
+
+
+}
